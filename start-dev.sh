35e8fc32aeffcbb3746355de07cd33a85bd11212
Alastair McFarlane <arm@apache.org>
2025-12-10 16:18:43 +0000

    Add missing sections for local dev compose, and re-add syft to Docker container

---
+++ start-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+# shellcheck source=/dev/null
+source .venv/bin/activate
+
+test -d /opt/atr/state || mkdir -p /opt/atr/state
+
+if [ ! -f state/cert.pem ] || [ ! -f state/key.pem ]
+then
+  python3 scripts/generate-certificates
+fi
+
+echo "Starting hypercorn on ${BIND}" >> /opt/atr/state/hypercorn.log
+exec hypercorn --reload --bind "${BIND}" \
+  --keyfile key.pem --certfile cert.pem atr.server:app >> /opt/atr/state/hypercorn.log 2>&1
